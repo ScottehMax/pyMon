2e69e4b4e46dc65ddd221c4d70b26e1fa28eb069
Scott Maxwell <wsemax@gmail.com>
2015-05-08 23:08:31 +0100

    Update utils

--- utils.py
+++ utils.py
@@ -36,6 +36,58 @@ def print_info(obj):
         print attrib, '-', getattr(obj, attrib)
 
 
+def get_thread():
+    catalog_uri = "http://a.4cdn.org/vp/catalog.json"
+
+    r = requests.get(catalog_uri)
+
+    try:
+        catalog = json.loads(r.text)
+    except ValueError:
+        print "Unable to decode the response"
+        return False
+
+    newest_thread = {'no': 0, 'last_reply_time': 0}
+
+    for page in catalog:
+        for thread in page['threads']:
+            if ((thread.get('sub') and 'showderp' in thread.get('sub')) or
+                    (thread.get('name') and 'showderp' in thread.get('name')) or
+                    (thread.get('com') and 'showderp' in thread.get('com'))) and thread.get('last_replies'):
+
+                reply_time = thread['last_replies'][-1]['time']
+
+                if reply_time > newest_thread['last_reply_time']:
+                    newest_thread['no'] = thread['no']
+                    newest_thread['last_reply_time'] = reply_time
+
+    return newest_thread
+
+
+def get_battle(ch):
+        battle_regex = "(https?\\:\\/\\/play\\.pokemonshowdown\\.com\\/battle-(?:FORMATS)\\-\\d+)"
+        real_regex = re.compile(battle_regex.replace('FORMATS', '|'.join(ch.battle_formats)))
+
+        thread_info = get_thread()
+
+        battles = []
+
+        if thread_info['no'] == 0:
+            return False
+
+        r = requests.get('http://a.4cdn.org/vp/res/%s.json' % thread_info['no'])
+
+        thread = json.loads(r.text)
+
+        for post in thread['posts']:
+            if post.get('com'):
+                m = real_regex.search(post.get('com').replace('<wbr>', ''))
+                if m:
+                    battles.append([m.group(), post['time']])
+
+        return battles
+
+
 def load_js_obj_literal(j):
     """Terrible hack."""
     j = j[j.index('{'):]
