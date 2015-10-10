60eee943b9e68f3197092d9be8e619e8c96619a3
Scott Maxwell <scott@scotteh.me>
2015-10-10 15:26:39 +0100

    Fix style a little

--- connect.py
+++ connect.py
@@ -15,7 +15,8 @@ def runbot(t):
         ws.connect()
         ws.run_forever()
     except Exception as e:
-        print 'Exception: {0}\nArguments:\n{1!r}'.format(type(e).__name__, e.args)
+        print 'Exception: {0}\nArguments:\n{1!r}'.format(type(e).__name__,
+                                                         e.args)
         print 'Unable to connect. Timing out for %s seconds...' % t
         time.sleep(t)
         runbot(t+2)
@@ -24,12 +25,15 @@ if __name__ == '__main__':
     while True:
         try:
             if 'chatbot' not in [x.name for x in threading.enumerate()]:
-                bot = threading.Thread(target=runbot, name='chatbot', args=([2]))
+                bot = threading.Thread(target=runbot,
+                                       name='chatbot',
+                                       args=([2]))
                 # Entire program exits when there are only daemon threads
                 bot.daemon = True
                 bot.start()
             time.sleep(10)
         except (KeyboardInterrupt, SystemExit):
-            # Entire program will exit, since MainThread is the only non-daemon thread
-            # The sole purpose of this is so CTRL+C etc. will close the whole program
+            # Entire program will exit, since MainThread is the only
+            # non-daemon thread -- The sole purpose of this is so CTRL+C etc.
+            # will close the whole program
             exit()
